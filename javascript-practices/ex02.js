670e377a2d58fa786f87be4ba122c12050170551
wow1186 <WOW1186@NAVER.COM>
2021-06-25 16:04:31 +0900

    ex02

--- javascript-practices/ex02.js
+++ javascript-practices/ex02.js
@@ -1,28 +1,55 @@
 /*
 변수와 데이터 타입
-
-[기본타입 (primitive type) , 유사 객체]
+[기본타입(primitive type), 유사객체]
 1. number
 2. boolean
 3. string
 4. undefined
-
-[객체]
-Number()
-Boolean()
-String()
-
-
+[객체1] - object type 
+1. new 생성자함수() 사용해서 생성,
+    Number()  -> object type 객체 생성
+    Boolean() -> object type 객체 생성
+    String()  -> object type 객체 생성
+    Object()  -> object type 객체 생성
+    Array()   -> object type 객체 생성
+2. null
+3. [](배열 리터럴)
+4. {}(객체 리터럴, JSON)
+ 
+[객체2] - function type 
 */
-a = 10;
-var u; 
-// var u = undefined; 와 동일 자바스크립트 선언
+
+console.log("==== 원시 타입 =====================")
+var u = undefined; // var u; 와 동일 선언과 정의의 구분이 없다. 
 var i = 10;
 var b = true;
-var s = "hello world";
-console.log("u : " + typeof(u));
-console.log("i : " + typeof(i));
-console.log("b : " + typeof(b));
-console.log("s: " + typeof(s));
+var s = "Hello World";
 
-console.log(new String(s).toUpperCase(s));
+console.log("u :" + typeof(u));
+console.log("i :" + typeof(i));
+console.log("b :" + typeof(b));
+console.log("s :" + typeof(s));
+
+console.log("====[객체1]: object type =====================")
+var i2 = new Number(10);
+var b2 = new Boolean(true);
+var s2 = new String("Hello World");
+var o = new Object(true);
+var a = new Array();
+var n = null;
+var a2 = [];
+var o2 = {};
+
+console.log("i2 :" + typeof(i2));
+console.log("b2 :" + typeof(b2));
+console.log("s2 :" + typeof(s2));
+console.log("o2 :" + typeof(o));
+console.log("a2 :" + typeof(a));
+console.log("o2 :" + typeof(n));
+console.log("a2 :" + typeof(a2));
+console.log("a2 :" + typeof(o2));
+
+
+console.log("====[객체2]: function type =====================")
+
+var n = null;
