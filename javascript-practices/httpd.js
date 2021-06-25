937b9bf6768ce7110f086ed6013b022f2e5db5c5
wow1186 <WOW1186@NAVER.COM>
2021-06-25 14:20:00 +0900

    httpd.js # add

---
+++ javascript-practices/httpd.js
@@ -0,0 +1,14 @@
+var port = 7070;
+
+var http = require("http");
+
+var server = http.createServer(function(req,resp){
+resp.writeHead(200,{ 
+"Content-Type": "text/html"
+});
+resp.end("<h1>hello wowrld</h1>");
+
+}).listen(port,function(){
+console.log("listing on" + port);
+});
+
