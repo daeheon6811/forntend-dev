a06eea9f717e432b62f8d259ddeee6d5c6a4a614
wow1186 <WOW1186@NAVER.COM>
2021-06-28 10:27:23 +0900

    ex03 #add

---
+++ javascript-practices/ex03.js
@@ -0,0 +1,21 @@
+/* null vs undefined */
+//var myVar1; = undefine; 명시적으로 underfined 대입 
+
+var myVar1; // 암시저긍로 underfined 대입
+var myVar2 = null;
+
+console.log(myVar1 + ":" + myVar2);
+console.log(typeof(myVar1) + ":" + typeof(myVar2));
+
+var a= { test : 1 , 
+       test2 : 2}
+
+  
+
+console.log(myVar1 == myVar2); // 값 비교
+
+
+var i = 10;
+i = 10;
+
+var u;
