4edea63afd7fd9bf4b2c5c9063dfdc3a37867abc
wow1186 <WOW1186@NAVER.COM>
2021-06-29 13:34:12 +0900

    ex #add

--- javascript-practices/ex05.js
+++ javascript-practices/ex05.js
@@ -1,43 +1,41 @@
-/* 
-구문(Statement) : 자바스클비트의 실행 단위
-
+/*
+구문(Statement)
 1. 자바스크립트의 실행 단위
 2. 구성 요소
-   값 , 연산자 = 표현식
-   주석 : 구문을 구성해도 실행 되지 안흔다
-   예약어 : if , else , for , ...
+   값, 연산자 : 표현식
+   주석       : 구문을 구성해도 실행되지 않는다.
+   예약어     : if, else, for, ...
 3. 구문의 예
-    if , if~else , if~elseif~else
-    while , do~while, for 
-    switch
-    for~in
-    with(deprecated)
-    변수 정의 (함수 정의)
-
-4. 공백: 토큰 구분 , 무시
-
+   if, if~else, if~elseif~else
+   while, do-while, for
+   switch
+   for~in
+   with(deprecated)
+   변수 정의(함수 정의)
+4. 공백: 토큰 구분, 무시   
 5. 세미콜론:
-   원친적으로 구문을 분리하는 역활
-
+   원칙적으로 구문을 분리하는 역할 
+6. 개행(line breaker)   
 */
 
+// ;는 
+// 표현식 var s = "hello World" 를 
+// 표현식 구문(Expression Statement)으로 만들어
+// 표현식을 실행하게끔 한다.
+var s = "hello World"; console.log(s)
 
-// 1. 표현식 구문을 만드는 역활을 한다.
-var s = "hello World";
-console.log(s);
-
-// 개행은
-var s = "hello World";
-console.log(s);
+// 1. 개행은 ;으로 대체된다.
+var s = "hello World"
+console.log(s)
 
 // 2.
-// 상황에 따라서는 토큰을 분리하는 역활도 하고
-// ; 한다
-
-
+// 하지만
+// 상황에 따라서는 토큰을 분리하는 역할도 하고
+// ; 역할도 한다.
 a
 =
 2
 +
 2
-console.log(a);
+console.log(a)
+
--- javascript-practices/ex08.js
+++ javascript-practices/ex08.js
@@ -1,50 +1,41 @@
 /*
 자바스크립트 객체 3
-
 */
-
 var o = {
-    name : "둘리",
-    age : 10,
-    isFemale : false
+    name: "둘리",
+    age: 10,
+    isFemale: false
 }
+console.log(o);
 
 var f = function(){
-    console.log("Hello World")
+    console.log("Hello World");
 }
-
-
-
 f();
 
-console.log(o);
-console.log("===객체의 확장===================");
-o.anthoer ={
-    name : "마이클",
-    age: 30 
+var i1 = 10;
+var i2 = new Number(10);
+
+console.log("===객체의 확장=====================");
+o.another = {
+    name: "마이콜",
+    age: 30
 };
-
 console.log(o);
 
-f.anthoer ={
-    name : "마이클",
-    age: 30, 
+f.another = {
+    name: "마이콜",
+    age: 30,
     info: function(){
-                console.log("info 호출 ! "+ this.name + ":" + this .age);
+        console.log(this.name + ":" + this.age);
     }
 };
+console.log(f.another);
+f.another.info();
 
-
-
-console.log(f.anthoer)
-f.anthoer.info();
-
-
-console.log("==== 기본 타입은 확장 X ===================");
-
-i2.anthoer ={};
+console.log("===기본 타입은 확장X=====================");
+i2.another = {};
 console.log(i2);
 
-i1.anthoer ={};
-console.log(i1.anthoer);
-
+i1.another = {};
+console.log(i1.another);
