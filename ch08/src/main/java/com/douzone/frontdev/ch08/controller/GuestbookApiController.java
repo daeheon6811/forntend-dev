31688d47657c549e6f3f817e2f641f0bbc82bba9
wow1186 <WOW1186@NAVER.COM>
2021-07-07 17:39:08 +0900

    ajax !!

---
+++ ch08/src/main/java/com/douzone/frontdev/ch08/controller/GuestbookApiController.java
@@ -0,0 +1,80 @@
+package com.douzone.frontdev.ch08.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.douzone.frontdev.ch08.dto.JsonResult;
+import com.douzone.frontdev.ch08.vo.GuestBookVo;
+
+@RequestMapping("/guestbook/api")
+@Controller
+public class GuestbookApiController {
+	
+	@ResponseBody
+	@RequestMapping("/list")
+	public JsonResult ex1() {
+		 List<GuestBookVo> list = new ArrayList<GuestBookVo>();
+		 
+		 GuestBookVo vo3 = new GuestBookVo();
+		 vo3.setNo(1L);
+		 vo3.setName("둘리");
+		 vo3.setMessage("호이1");
+		 list.add(vo3);
+		 
+		 GuestBookVo vo2 = new GuestBookVo();
+		 vo2.setNo(1L);
+		 vo2.setName("둘리2");
+		 vo2.setMessage("호이2");
+		 list.add(vo2);
+		 
+		 GuestBookVo vo1 = new GuestBookVo();
+		 vo1.setNo(2L);
+		 vo1.setName("둘리1");
+		 vo1.setMessage("호이1");
+		 list.add(vo1);
+		 
+		 
+		 
+		 return JsonResult.success(list);
+	}
+	
+	
+	@ResponseBody
+	@RequestMapping("/add")
+	public JsonResult ex2(@RequestBody GuestBookVo vo) {
+        // 등록 작업 ()
+		
+		vo.setNo(11L);
+		vo.setPassword("");
+		
+		return JsonResult.success(vo);
+	    
+		
+	}
+	@ResponseBody
+	@RequestMapping("delete/{no}")
+	public JsonResult ex2(@PathVariable("no") Long no , String password) {
+        // 삭제 작업 (GuestbookService)
+		Long data = 0L;
+
+		//  1. 삭제가 안된 경우
+		data = -1L;
+		
+		// 2 삭제가 된 경우
+		data = no;
+		return JsonResult.success(data);
+	    
+		
+	}
+	
+	
+	
+	
+}
+
